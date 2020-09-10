7f7252810118a72edc28f6c1e74f4d93971992e6
Agualuza <iagoagualuza@id.uff.br>
2020-09-10 01:37:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ io.on('connection', async socket => {
 				       +'"datetime"  : '+utils.data.getTimestamp()+','
 				       +'"user_id" : '+data.user_id_1+''
 				       +'}';
-		socket.send(data.message);
+		socket.send(message);
 	});
 });
 
