5b8e39d20199c40b6c5499782f9664adbe8b0fc2
Agualuza <iagoagualuza@id.uff.br>
2020-09-10 01:24:30 -0300

    Arruma a conexão com redis para o ambientes de desenvolvimento no heroku

--- redis.js
+++ redis.js
@@ -1,7 +1,7 @@
 const redis = require('redis'),
   client    = redis.createClient({
     port      : 6379,               
-    host      : '127.0.0.1',        
+    host      : process.env.REDISCLOUD_URL,        
     // password  : '',    
    
   });
